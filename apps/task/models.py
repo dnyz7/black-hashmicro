6083b34448104f154f52ef1a164ba91aeefa81ec
Denny Husniansyah <denny.husniansyah@gmail.com>
2023-03-08 19:23:20 +0700

    update task

--- apps/task/models.py
+++ apps/task/models.py
@@ -5,8 +5,8 @@ class Task(db.Model):
     __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,db.ForeignKey('Users.id'))
-    task_title = db.Column(db.String(64) ,nullable=False ,unique=False)
+    task_name = db.Column(db.String(64) ,nullable=False ,unique=False)
     task_info = db.Column(db.String(256) ,nullable=False,unique=False)
     # 0 = on Doing || 1 = Done! 
     status = db.Column(db.Integer ,default = 0)
-    date = db.Column(db.DateTime ,default=datetime.now())
+    timestamp = db.Column(db.DateTime ,default=datetime.now())
--- apps/task/routes.py
+++ apps/task/routes.py
@@ -1,6 +1,6 @@
 from apps.task import blueprint
-from flask import render_template, request,flash, redirect
-from flask_login import login_required
+from flask import render_template, request,flash, redirect,url_for
+from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
 
 from apps import db, login_manager
@@ -9,32 +9,45 @@ from apps.task.forms import TaskForm
 from apps.task.models import Task
 from apps.authentication.models import Users
 
-from flask_login import current_user
-
-@blueprint.route('/task')
+@blueprint.route('/task', methods=['GET', 'POST'])
 @login_required
 def index():
     create_task_form = TaskForm(request.form)
-    return render_template('task/index.html', segment='task',form=create_task_form)
+    if 'task' in request.form:
+        info = request.form["task_info"]
+        name = request.form["task_name"] 
+        result = add_task(current_user.id,name.title(),info.title())
 
-@blueprint.route("/add_task",methods=['POST'])
-def add_new_task():
-    # task_form = TaskForm(request.form)
-    title = request.form("task_info")
-    info = request.form("task_name")   
+        return redirect(url_for('task_blueprint.index'))
+        # return render_template('task/index.html',
+        #                        result,user_list_task = list_task(),
+        #                        form=create_task_form)
+    else:
+       
+        return render_template('task/index.html', user_list_task = list_task(), segment='task',form=create_task_form)
 
-    # if not title.strip():
-    #     return redirect('/')
-    # if not info.strip:
-    #     return redirect('/')
-    # task = Task(**request.form)
-
-    # add task to user db
-    new_task = Task(user_id=current_user['user_id'],task_info=info.title(),task_title=title.title())
+@blueprint.route("/list_task",methods=['GET'])
+def list_task():
     try:
-        db.session.add(new_task)
-        db.session.commit()
-        flash("Task Added SuccessFully")
-        return redirect('/')
+        user_task = []
+        user_db_data = Task.query.filter_by(user_id=current_user.id).all()
+        # user_db_data = Task.query.filter_by(user_id=current_user.id).order_by(timestamp.desc()).all()
+        for index,value in enumerate(user_db_data):
+            print(value.status)
+            if value.status != 1:
+                user_task.append(value)
+        return user_task
     except:
+        return render_template('home/page-500.html'), 500
+    
+@blueprint.route("/add_task",methods=['POST'])
+def add_task(user_id,task_name, task_info):
+    try:
+        add_task = Task(user_id=user_id,task_info=task_info.title(),task_name=task_name.title())
+        db.session.add(add_task)
+        db.session.commit()
+        result = {"msg":"Task created successfully", "succes":True}
+        return result
+    except:
+        result = {"msg":"Create task failed", "succes":False}
         return render_template('home/page-500.html'), 500
